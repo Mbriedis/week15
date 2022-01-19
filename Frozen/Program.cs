af01d380136bb89cbff5df5942e81edae3e4328e
Martins Briedis <mbriedis@tktk.ee>
2022-01-19 05:53:17 +0200

    week 9

---
+++ Frozen/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+    //anna wants new earrings for christmas 
+    class Present
+    {
+        string name;
+        string gift;
+        public Present(string _name, string _gift)
+        {
+            name = _name;
+            gift = _gift;
+
+        }
+        public string Name
+        {
+            get { return name; }
+        }
+        public string Gift
+        {
+            get { return gift; }
+        }
+
+    }
+        static void Main(string[] args)
+        {
+            List<Present> mygifts = new List<Present>();
+            string[] newgiftfromFile = GetDataFromFile();
+
+        foreach (string line in newgiftfromFile)
+            {
+            string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            Present newGift = new Present(tempArray[0], tempArray[1]);
+            mygifts.Add(newGift);
+            }
+            foreach (Present GiftsfromList in mygifts)
+            {
+                Console.WriteLine($" {GiftsfromList.Name} wants {GiftsfromList.Gift} for Christmas");
+            }
+
+
+
+        }
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+
+            foreach (string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+        public static string[] GetDataFromFile()
+
+                 {     
+        
+            string filepath = @"C:\Users\marti\samples\frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filepath);
+
+            return dataFromFile;
+
+
+
+                   }
+
+
+
+
+
+
+    } 
+
---
+++ opAndFiles/Program.cs
@@ -0,0 +1,75 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class program
+{
+
+    class Movie
+    {
+        string title;
+        string rating;
+        string year;
+
+        public Movie(string _title, string _rating, string _year)
+        {
+            title = _title;
+            rating = _rating;
+            year = _year;
+        }
+        //getters for movie
+        public string Title
+        {
+            get { return title; }
+        }
+        public string Rating
+        {
+            get { return rating; }
+        }
+        public string Year
+        {
+            get { return year; }
+        }
+    }
+    static void Main(string[] args)
+    {
+        List<Movie> myMovies = new List<Movie>();
+        string[] moviesFromFile = GetDataFromFile();
+
+        foreach (string line in moviesFromFile)
+        {
+            string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            Movie newMovie = new Movie (tempArray[0], tempArray[1],tempArray[2]);
+            myMovies.Add(newMovie); 
+        }
+        foreach (Movie movieFromList in myMovies)
+        {
+            Console.WriteLine($"Title --> {movieFromList.Title}. Rating --> {movieFromList.Rating}.Year --> {movieFromList.Year}");
+        }
+
+
+
+    }
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+
+            foreach (string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+            public static string[] GetDataFromFile()
+            {
+                string filepath = @"C:\Users\marti\samples\movies.txt";
+                string[] dataFromFile = File.ReadAllLines(filepath);
+
+                return dataFromFile;
+
+
+
+            }
+
+
+
+
+
+        
+    }
